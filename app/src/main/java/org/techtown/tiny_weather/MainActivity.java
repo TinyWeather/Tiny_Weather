2398e757fb68a814c55985988fe8fb7a5031adf4
윤채린 <youncr@naver.com>
2020-09-10 23:10:32 +0900

    +) 로딩화면, 액션바, 네비게이션 드로어

--- app/src/main/java/org/techtown/tiny_weather/MainActivity.java
+++ app/src/main/java/org/techtown/tiny_weather/MainActivity.java
@@ -1,12 +1,22 @@
 package org.techtown.tiny_weather;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements onTabItemSelectedListener {
 
@@ -18,10 +28,23 @@ public class MainActivity extends AppCompatActivity implements onTabItemSelected
 
     BottomNavigationView bottomNavigationView;
 
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+
+    private DrawerLayout mDrawerLayout;
+    private Context context = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 로딩
+        Intent intent = new Intent(this, LodingActivity.class);
+        startActivity(intent);
+
+        // 액션바 + 네비게이션 드로우
+        setActionBar();
 
         mapFragment = new MapFragment();
         covidFragment = new CovidFragment();
@@ -58,6 +81,70 @@ public class MainActivity extends AppCompatActivity implements onTabItemSelected
             }
         });
     }
+
+    private void setActionBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false); // 기존 title 지우기
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 만들기
+        actionBar.setHomeAsUpIndicator(R.mipmap.menu); //뒤로가기 버튼 이미지 지정
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();
+
+                int id = menuItem.getItemId();
+                String title = menuItem.getTitle().toString();
+
+                if(id == R.id.profile){
+                    Toast.makeText(context, title, Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.push){
+                    Toast.makeText(context, title, Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.gps){
+                    Toast.makeText(context, title, Toast.LENGTH_SHORT).show();
+                }
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ // 왼쪽 상단 버튼 눌렀을 때
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+        }
+    };
 
     @Override
     public void onTabSelected(int position) {
