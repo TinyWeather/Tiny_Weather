8876eb5732465b0cd5abbfd9ae38416667cc614f
윤채린 <youncr@naver.com>
2020-09-12 20:42:37 +0900

    +) 홈 화면 위치 정보 받아오기

--- app/src/main/java/org/techtown/tiny_weather/HomeFragment.java
+++ app/src/main/java/org/techtown/tiny_weather/HomeFragment.java
@@ -1,26 +1,117 @@
 package org.techtown.tiny_weather;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.zip.Inflater;
+
 public class HomeFragment extends Fragment {
+    private LocationManager locationManager;
+    private static final int REQUEST_CODE_LOCATION = 2;
+    ViewGroup rootView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_home, container, false);
+        rootView = (ViewGroup) inflater.inflate(R.layout.activity_home, container, false);
 
         initUI(rootView);
 
         return rootView;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //사용자의 위치 수신을 위한 세팅
+        locationManager = (LocationManager)getContext().getSystemService(Context.LOCATION_SERVICE);
+        //사용자의 현재 위치
+        Location userLocation = getMyLocation();
+        if( userLocation != null ) {
+            double latitude = userLocation.getLatitude();
+            double longitude = userLocation.getLongitude();
+            System.out.println("★★★ 현재 내 위치값 : "+latitude+","+longitude+" ★★★");
+            String address = getAddress(getActivity().getBaseContext(), latitude, longitude);
+
+            System.out.println("★★★ 현재 내 위치값 : "+address+" ★★★");
+            TextView textView = (TextView) rootView.findViewById(R.id.txt_location2);
+            if(!address.equals("주소를 가져올 수 없습니다.") && !address.equals("현재 위치를 확인 할 수 없습니다.")) {
+                String strAddress[] = address.split(" ");
+                textView.setText(strAddress[2] + " " + strAddress[3]);
+            }
+            else {
+                textView.setText("위치 조회 불가");
+            }
+        }
+    }
+
     public void initUI(ViewGroup rootView) {
 
+    }
+
+    /* 사용자의 위치를 수신 */
+    private Location getMyLocation() {
+        Location currentLocation = null;
+        // Register the listener with the Location Manager to receive location updates
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("★★★ 사용자에게 권한을 요청해야함 ★★★");
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, this.REQUEST_CODE_LOCATION);
+            getMyLocation(); //이건 써도되고 안써도 되지만, 전 권한 승인하면 즉시 위치값 받아오려고 썼습니다!
+        }
+        else {
+            System.out.println("★★★ 권한요청 안해도됨 ★★★");
+
+            // 수동으로 위치 구하기
+            String locationProvider = LocationManager.GPS_PROVIDER;
+            currentLocation = locationManager.getLastKnownLocation(locationProvider);
+            if (currentLocation != null) {
+                double lng = currentLocation.getLongitude();
+                double lat = currentLocation.getLatitude();
+            }
+        }
+        return currentLocation;
+    }
+
+    /* 위도,경도로 주소구하기 */
+    public static String getAddress(Context mContext, double lat, double lng) {
+        String nowAddress ="현재 위치를 확인 할 수 없습니다.";
+        Geocoder geocoder = new Geocoder(mContext, Locale.KOREA);
+        List<Address> address;
+        try {
+            if (geocoder != null) {
+                //세번째 파라미터는 좌표에 대해 주소를 리턴 받는 갯수로
+                //한좌표에 대해 두개이상의 이름이 존재할수있기에 주소배열을 리턴받기 위해 최대갯수 설정
+                address = geocoder.getFromLocation(lat, lng, 1);
+
+                if (address != null && address.size() > 0) {
+                    // 주소 받아오기
+                    String currentLocationAddress = address.get(0).getAddressLine(0).toString();
+                    nowAddress  = currentLocationAddress;
+                }
+            }
+        } catch (IOException e) {
+            nowAddress = "주소를 가져올 수 없습니다.";
+            System.out.println("주소를 가져올 수 없습니다.");
+            e.printStackTrace();
+        }
+        return nowAddress;
     }
 }
