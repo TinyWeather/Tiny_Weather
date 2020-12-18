1a49ad296998395cc55141b18160283c4005b711
지원 호 <linaj15@naver.com>
2020-12-18 11:16:42 +0900

    회원가입시 한번 되묻기

--- app/src/main/kotlin/org/techtown/tiny_weather/Login.kt
+++ app/src/main/kotlin/org/techtown/tiny_weather/Login.kt
@@ -1,8 +1,10 @@
 package org.techtown.tiny_weather
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -21,28 +23,46 @@ class Login : AppCompatActivity() {
         startActivity(intent)
 
         join_btn.setOnClickListener {
-            if(email.text.toString() == "" || password.text.toString() == ""){
+            if(email.text.toString() != "" || password.text.toString() != ""){
                 login()
+            }else{
+                Toast.makeText(this, "빈칸을 채우세요", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
     fun login(){
-        auth?.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
-                ?.addOnCompleteListener { task ->
-                    if(task.isSuccessful){
-                        //Creating a user account
-                        goHome(task.result?.user)
-                    } else{
-                        signin()
-                    }
-                }
+
+        var builder = AlertDialog.Builder(this)
+        builder.setTitle("회원가입")
+        builder.setMessage("회원가입하시겠습니까?")
+        builder.setIcon(R.mipmap.ic_launcher_logo)
+
+        var listener = DialogInterface.OnClickListener{
+            _,p1 ->
+            when(p1){
+                DialogInterface.BUTTON_POSITIVE->
+                    auth?.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                            ?.addOnCompleteListener { task ->
+                                if(task.isSuccessful){
+                                    //Creating a user account
+                                    goHome(task.result?.user)
+                                } else{
+                                    signin()
+                                }
+                            }
+                DialogInterface.BUTTON_NEGATIVE ->
+                    Toast.makeText(this, "미 가입시 앱 이용 불가", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        builder.setPositiveButton("예",listener)
+        builder.setNegativeButton("아니요",listener)
+
+        builder.show();
     }
 
     fun signin(){
-
-
-
         auth?.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
                 ?.addOnCompleteListener { task ->
                     if(task.isSuccessful){
